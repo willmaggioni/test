2ecdcee5da51e22ff57d76fa87c71202080a1ea3
willmaggioni <will.maggioni@gmail.com>
2026-03-07 01:20:30 +0100

    Delete hello.ps1

--- hello.ps1
+++
@@ -1,6 +0,0 @@
-<#
-  Comendo per rendere eseguibili script powershell nella sessione corrente:
-  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process.
-#>
-
-Write-Host "Hello, World!"
